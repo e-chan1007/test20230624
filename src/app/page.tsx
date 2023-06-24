3c856014467789d842251cd43cb28cca04437898
いーちゃん(e_chan1007) <echan041007@gmail.com>
2023-06-24 23:24:31 +0900

    feat: show permission prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,10 @@ export default function Home() {
   const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
   const [shakeCount, setShakeCount] = useState(0);
   const [timeoutId, setTimeoutId] = useState<number | null>(null);
+  const [isPermissionGranted, setIsPermissionGranted] = useState(false);
 
-  const start = () => {
+  useEffect(() => {
+    if(!isPermissionGranted) return;
     const listener = (event: DeviceMotionEvent) => {
       const { x, y, z } = event.acceleration || { x: 0, y: 0, z: 0};
       if(Math.abs(x ?? 0) + Math.abs(y ?? 0) + Math.abs(z ?? 0) > 15) {
@@ -29,16 +31,20 @@ export default function Home() {
     };
     window.addEventListener("devicemotion", listener)
     return () => { window.removeEventListener("devicemotion", listener)}
+  }, [isPermissionGranted]);
+
+  const requestPermission = async () => {
+    const result = await (DeviceMotionEvent as any).requestPermission();
+    setIsPermissionGranted(result === "granted");
   }
 
   useEffect(() => {
     if (
-      window.DeviceMotionEvent &&
-      (window.DeviceMotionEvent as any).requestPermission
+      (window.DeviceMotionEvent as any)?.requestPermission
     ) {
-      (DeviceMotionEvent as any).requestPermission().then(start);
+      (DeviceMotionEvent as any).requestPermission().then((result: string) => setIsPermissionGranted(result === "granted"))
     } else {
-      start();
+      setIsPermissionGranted(true);
     }
   }, []);
 
@@ -51,24 +57,31 @@ export default function Home() {
   return <main className="relative w-screen h-screen">
     <div className="z-0 absolute w-full h-full transition-all"  style={{ backgroundColor: "#EE8000", clipPath }} />
     <div className="z-10 absolute w-full h-full flex flex-col items-center justify-center">
-      <h1 className="text-lg">加速度</h1>
-      <table>
-        <tbody>
-          <tr>
-            <th className="px-4">X</th>
-            <td className="px-4">{acceleration.x}</td>
-          </tr>
-          <tr>
-            <th className="px-4">Y</th>
-            <td className="px-4">{acceleration.y}</td>
-          </tr>
-          <tr>
-            <th className="px-4">Z</th>
-            <td className="px-4">{acceleration.z}</td>
-          </tr>
-        </tbody>
-      </table>
-      <p className="text-4xl top-1/2 left-1/2">{shakeCount}</p>
+      {
+        isPermissionGranted ? (
+          <div className="flex flex-col gap-4 items-center">
+            <h1 className="text-2xl">加速度</h1>
+            <table>
+              <tbody>
+                <tr>
+                  <th className="px-4">X</th>
+                  <td className="px-4">{acceleration.x}</td>
+                </tr>
+                <tr>
+                  <th className="px-4">Y</th>
+                  <td className="px-4">{acceleration.y}</td>
+                </tr>
+                <tr>
+                  <th className="px-4">Z</th>
+                  <td className="px-4">{acceleration.z}</td>
+                </tr>
+              </tbody>
+            </table>
+            <p className="text-4xl top-1/2 left-1/2">{shakeCount}</p>
+          </div>
+        ) : (
+          <button onClick={requestPermission} className="px-4 py-2 border border-orange-700 text-orange-700 rounded hover:bg-orange-50 select-none">&quot;動作と方向&quot;へのアクセスを許可する</button>
+        )}
     </div>
   </main>
 }
